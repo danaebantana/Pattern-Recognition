c28f57a92825aac8fb4e0fd1ebbeddcec53444be
danaebantana <danae.bantana@hotmail.com>
2021-09-01 14:02:38 +0300

    dataSetUp done

--- dataSetUp.py
+++ dataSetUp.py
@@ -2,26 +2,39 @@ import functions as fun
 
 Match = []
 Match_Results = []
-companies_odds = [[]for x in range(4)]
-companies = ["B365","BW","IW","LB"]
+Match_Teams_Result = []
+Companies = ["B365","BW","IW","LB"]
+Companies_odds = [[]for x in range(4)]
+Attributes = []
+TeamAttributes= []
 
-with open("./kaggle_soccer_csv_matlab/Match.csv","r") as match_csv:
-    for line in match_csv:
+
+with open("./Data/Match.csv","r") as matchData:
+    for line in matchData:
         Match.append(line[:-1].split(","))
-
-
+del Match[0]
 
 num_of_matches = len(Match)
-num_of_companies = len(companies_odds)
+num_of_companies = len(Companies)
 
 for m in Match:
-    home_goals = m[10]
-    away_goals = m[11]
-    result = fun.match_result(home_goals,away_goals)
-    Match_Results.append([int(m[6]),result])            #Match_Results stores the match result for each match_id. Match_Results[match,result]
+    Gmh = m[9]   #Home Goals
+    Gma = m[10]  #Away Goals
+    result = fun.match_result(Gmh,Gma)
+    Match_Results.append([int(m[6]),result])   #[match_api_id, result]     
+    Match_Teams_Result.append([int(m[7]), int(m[8]), result])   #[home_team_api_id, away_team_api_id, result]  
 
 index = 11
 for company in range(num_of_companies):
-    for m in range(num_of_matches):
-        companies_odds[company].append([int(Match[m][6]),float(Match[m][index]), float(Match[m][index+1]), float(Match[m][index+2])])
+    for match in range(num_of_matches):
+        # [match_api_id, name_of_companyH, name_of_companyD, name_of_companyA]
+        Companies_odds[company].append([int(Match[match][6]),float(Match[match][index]), float(Match[match][index+1]), float(Match[match][index+2])])
     index += 3
+    
+with open("./Data/TeamAttributes.csv","r") as teamAttributesData:
+    for line in teamAttributesData:
+        Attributes.append(line[:-1].split(","))
+del Attributes[0]    
+        
+for t in Attributes:
+    TeamAttributes.append([int(t[4]), int(t[5]), int(t[6]), int(t[7]), int(t[8]), int(t[9]), int(t[10]), int(t[11])])
--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep  1 12:51:18 2021
-
-@author: Danae
-"""
-
+def match_result(Gmh,Gma):
+    diff_goal = int(Gmh) - int(Gma)
+    if(diff_goal > 0):
+        return "H"
+    elif(diff_goal == 0):
+        return "D"
+    else:
+        return "A"
